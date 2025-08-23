400d5960af3410b12e1ea070b8f1887fa99dc5e7
Heshan Padmasiri <hpheshan@gmail.com>
2025-08-23 19:09:06 +0530

    Setup nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "SimpleMediaServer Go development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+            fish
+          ];
+
+          shellHook = ''
+            export SHELL=${pkgs.fish}/bin/fish
+            exec ${pkgs.fish}/bin/fish
+          '';
+        };
+      });
+}
